dfb2a090b81475ee99730f7866e27e93b8b788ce
kratikpal <kratikpal@gmail.com>
2025-05-26 13:42:18 +0530

    nojekyll added

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const repositoryName = 'kratikpal.github.io'; 
-
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: `/${repositoryName}/`
+  base: '/',
 });
